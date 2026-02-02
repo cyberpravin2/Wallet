74a10b06071dab2f7fcd5f6d7e9d3cda9428db81
cyberpravin2 <pravin709121@gmail.com>
2026-02-02 23:05:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from pyrogram import Client
-from config import BOT_TOKEN
+
+from config import BOT_TOKEN, API_ID, API_HASH
 from handlers import start, wallet, admin, admin_roles
 
 app = Client(
     "monkey_wallet_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
     bot_token=BOT_TOKEN
 )
 
