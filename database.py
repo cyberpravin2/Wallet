aeb7a7166b96a30304a2f2e3df4f4cb74c24ab37
cyberpravin2 <pravin709121@gmail.com>
2026-02-02 22:47:28 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,21 +1,8 @@
 from pymongo import MongoClient
-from config import MONGO_URI, DB_NAME
+from config import MONGO_URI
 
 client = MongoClient(MONGO_URI)
-db = client[DB_NAME]
+db = client["monkey_wallet_bot"]
 
-# Users & Wallet
 users_col = db.users
 wallet_logs_col = db.wallet_logs
-
-# Money flows
-deposits_col = db.deposits
-withdraws_col = db.withdraws
-
-# Tasks
-tasks_col = db.tasks
-task_submissions_col = db.task_submissions
-
-# Disputes & Settings
-disputes_col = db.disputes
-settings_col = db.settings
